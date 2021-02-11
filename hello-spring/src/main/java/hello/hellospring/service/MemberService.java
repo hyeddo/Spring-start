cc8a41bef751ee237a56aec2adbe87c884dc1bfd
hyeddo <55426397+hyeddo@users.noreply.github.com>
2021-02-11 17:21:01 +0900

    210211 JDBC

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -6,6 +6,7 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.Console;
 import java.util.List;
 import java.util.Optional;
 
